27ecbbe1b6ca1512861316fbcffc51eb5127130b
Pranjal-mukherjee <pranjal.mukharjee@zemosolabs.com>
2024-02-06 17:34:31 +0530

    added maven assignment

---
+++ Assignment/src/main/java/com/linuxassignment/Assignment/AssignmentApplication.java
@@ -0,0 +1,15 @@
+package com.linuxassignment.Assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+		SpringApplication.run(AssignmentApplication.class, args);
+	}
+
+}
